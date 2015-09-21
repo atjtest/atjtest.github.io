c8cd9d8a3514d14cdbf9c83933357dd2f48b394c
hf <hfc@hf.com>
2015-09-20 22:54:40 -0500

    improvement on general.html.for.images.in.multiple.folders.ps1

--- processing/general.html.for.images.in.multiple.folders.ps1
+++ processing/general.html.for.images.in.multiple.folders.ps1
@@ -8,24 +8,39 @@
 #   desc3
 # NOTE: readme.txt should be using unicode encoding if it contains CHINESE chars!! lines start with "#" will be ignored
 
-$imgsubdirs='mia2.1', 'mia2.2'
+$imgsubdirs=@('mia2.1', 'mia2.2', 3, 4,5,6,7,8,9,10,11,12)
 $htmlfilename='mia22.html'
 
 $basedir='d:\Data\gitrepos\atjshop.github.io\'
 $outputfilename=$basedir+'html\'+$htmlfilename
+$welnumber = @(1,2,3,2,3,1,3,1,2)
 
 Write-Host $outputfilename 
 
+$htmlbar='<div class="progress"><div class="progress-bar progress-bar-info" style="width: 100%"></div></div>' # a bar to separate different products
+
+$iDirs=0 #number of folders processed
+$html=$htmlbar #html for all folders' image, productTitle and descriptions
+$htmltmp='<a name="p{3}"></a><div class="news"><div class="container"><h3>{0}</h3><hr/><div class="new-grids">{1}</div></div></div><div class="news"><div class="container">{2}</div></div>'
+#$htmltmp='<div class="news"><div class="container"><h3>{0}</h3><hr/><div class="new-grids">{1}</div></div></div><div class="news"><div class="container">{2}</div></div>'
+
+$indexhtml='' #html for the table of content
+$indexhtmltmp='<div class="col-md-4 welcome-grid"><a href="#p{0}"><div class="wel{2} hvr-bounce-to-bottom"><h4>{1}</h4></a></div></div>'
+
+$indexhtml1='' #holding the html for table of content, maximum 3 items
+
 #loop thru image folders
-$iDirs=0
-$html=''
-#$htmltmp='<div class="news"><div class="container"><h3><a name="#p{3}">{0}</a></h3><hr/><div class="new-grids">{1}</div></div></div><div class="news"><div class="container">{2}</div></div>'
-$htmltmp='<div class="news"><div class="container"><h3>{0}</h3><hr/><div class="new-grids">{1}</div></div></div><div class="news"><div class="container">{2}</div></div>'
 foreach($dir in $imgsubdirs)
 {
     $imgdir= $basedir +'images\' + $dir + '\' # folder path in windows
     $imgpath='/images/' + $dir +'/'     # image path in html
     Write-Host $imgdir ' ' $imgpath
+
+    if ( -not (Test-Path $imgdir) ){
+        Write-Warning  ("folder doesn't exist: " + $imgdir)
+        continue
+    }
+
     $files=Get-ChildItem -Path $imgdir –File
 
     $i=0
@@ -33,8 +48,9 @@ foreach($dir in $imgsubdirs)
     $htmlImgDivs=''
     $htmlImgDiv='<div class="col-md-4 view"><a href="{0}" data-lightbox=s><img src="{0}" class="img-responsive"/></a></div>'
     $htmldesc='<div class="news"><div class="container">'
+
     foreach ($file in $files)
-    {  
+    {      
       if($file.Name -match "readme.txt"){
         # read the content of readme.txt, first line is title, rest of them are descriptions
         $readme=$imgdir+'readme.txt'
@@ -74,13 +90,29 @@ foreach($dir in $imgsubdirs)
       } #end of handing each image file      
 
     } #end of each file in one folder
-    #$html+= [string]::Format($htmltmp, $productTitle, $htmlImgDivs, $htmldesc, $iDirs)
-    $html+= [string]::Format($htmltmp, $productTitle, $htmlImgDivs, $htmldesc)
-    $html+='<div class="progress"><div class="progress-bar progress-bar-info" style="width: 100%"></div></div>'
+    $html+= [string]::Format($htmltmp, $productTitle, $htmlImgDivs, $htmldesc, $iDirs)
+    $html+=$htmlbar
 
-    write-host '***dir:' $dir ', img:' $i
+    $indexhtml1+=[string]::Format($indexhtmltmp,$iDirs,$productTitle,$welnumber[$iDirs%9])
+
     $iDirs++
+
+    if($iDirs%3 -eq 0){
+        $indexhtml+='<div class="welcome"><div class="container">' + $indexhtml1 + '</div></div>' #surround every 3 items with div.
+        $indexhtml1=''
+    }
+    write-host '***dir:' $dir ', img:' $i
+
 } #end of each folder
+
+if($indexhtml1 -ne ''){
+    $indexhtml+='<div class="welcome"><div class="container">' + $indexhtml1 + '</div></div>' #surround every 3 items with div.
+}
+
+# $indexhtml='<div class="welcome"><div class="container">' + $indexhtml + '</div></div>'
+
+Write-Host $indexhtml
+if($iDirs -gt 1){ $html = $indexhtml + $html }
 
 #write html to file
 $stream = new-object 'System.IO.StreamWriter' -ArgumentList $outputfilename, $false
