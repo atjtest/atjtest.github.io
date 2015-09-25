427210c19d605dda0188abb1a145351217d0255d
hf <hfc@hf.com>
2015-09-24 22:46:28 -0500

    added 2015-10-EL.ps1

--- processing/2015-10-EL.ps1
+++ processing/2015-10-EL.ps1
@@ -1,4 +1,4 @@
-. "d:\Data\gitrepos\atjshop.github.io\processing\general-html.for.multiple.products.ps1"
+﻿. "d:\Data\gitrepos\atjshop.github.io\processing\general-html.for.multiple.products.ps1"
 $imgsubdirs=@('2015-09-EL')
 $htmlfilename='2015-09-EL-SET.html'
 $title = '2015年10月份雅诗兰黛超值套装'
