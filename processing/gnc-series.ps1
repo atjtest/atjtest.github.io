250b7e21ab215bff5ebd00e4c6861207222c47f2
hf <hfc@hf.com>
2015-09-26 14:42:57 -0500

    added gnc-series

---
+++ processing/gnc-series.ps1
@@ -0,0 +1,5 @@
+﻿. "d:\Data\gitrepos\atjshop.github.io\processing\general-html.for.multiple.products.ps1"
+$imgsubdirs=@('GNC-Grape-Seed','GNC-Super-Digestive-Enzymes','GNC-Lutein','GNC-Soy-Isoflavone')
+$htmlfilename='GNC-Series.html'
+$title = '键安喜GNC营养保健品'
+generate-html-for-products $imgsubdirs $htmlfilename $title 1
