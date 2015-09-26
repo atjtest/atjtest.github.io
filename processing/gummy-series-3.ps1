6395a75ee9f2afd6f31d063407f81952c2df4d6f
hf <hfc@hf.com>
2015-09-25 23:03:01 -0500

    added more products
    reduce padding for wel1,wel2,wel3

---
+++ processing/gummy-series-3.ps1
@@ -0,0 +1,5 @@
+﻿. "d:\Data\gitrepos\atjshop.github.io\processing\general-html.for.multiple.products.ps1"
+$imgsubdirs=@('gummy-vites','gummy-calc','gummy-omega3-dha')
+$htmlfilename='Gummy-vites-calc-dha.html'
+$title = '儿童保健品小熊糖小熊钙小熊DHA'
+generate-html-for-products $imgsubdirs $htmlfilename $title 1
---
+++ processing/health-products.ps1
@@ -0,0 +1,5 @@
+﻿. "d:\Data\gitrepos\atjshop.github.io\processing\general-html.for.multiple.products.ps1"
+$imgsubdirs=@('NatureMade-Fishoil-200x2','schiff-melatonin-3mg','NatureMade-Flaxseed-Oil','Trunature-grape-seed','Trunature-lutein','Trunature-Cranberry','NatureMade-CoQ10')
+$htmlfilename='nutrition-health.html'
+$title = '营养保健品系列'
+generate-html-for-products $imgsubdirs $htmlfilename $title 1
