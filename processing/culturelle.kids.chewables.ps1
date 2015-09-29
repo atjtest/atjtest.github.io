f975f190563daffec0ff87901cbd30b82fa3e0db
hf <hfc@hf.com>
2015-09-28 23:13:20 -0500

    hide top nav on index.html since the category list is displayed
    added culturelle-kids-chewables

---
+++ processing/culturelle.kids.chewables.ps1
@@ -0,0 +1,5 @@
+﻿. "d:\Data\gitrepos\atjshop.github.io\processing\general-html.for.multiple.products.ps1"
+$imgsubdirs=@('culturelle-kids-chewables')
+$htmlfilename='culturelle-kids-chewables.html'
+$title = 'Culturelle儿童益生菌含片30颗'
+generate-html-for-products $imgsubdirs $htmlfilename $title
