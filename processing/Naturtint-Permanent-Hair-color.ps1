6f72a64e97844ef7a73e8d3d7fee305602e1a990
hf <hfc@hf.com>
2015-09-30 00:19:58 -0500

    added Dyson-hot-cool-fan-AM05
    added Naturtint-Permanent-Hair

---
+++ processing/Naturtint-Permanent-Hair-color.ps1
@@ -0,0 +1,5 @@
+﻿. "d:\Data\gitrepos\atjshop.github.io\processing\general-html.for.multiple.products.ps1"
+$imgsubdirs=@('Naturtint-Permanent-Hair')
+$htmlfilename='Naturtint-Permanent-Hair.html'
+$title = '天然无毒有机染发剂'
+generate-html-for-products $imgsubdirs $htmlfilename $title
---
+++ processing/dyson-hot-cool-fan-am05.ps1
@@ -0,0 +1,5 @@
+﻿. "d:\Data\gitrepos\atjshop.github.io\processing\general-html.for.multiple.products.ps1"
+$imgsubdirs=@('Dyson-hot-cool-fan-AM05')
+$htmlfilename='Dyson-hot-cool-fan-AM05.html'
+$title = '戴森冷暖智能恒温超静音无叶风扇'
+generate-html-for-products $imgsubdirs $htmlfilename $title
