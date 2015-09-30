869f8b261b526639836d94e4e143206389331893
hf <hfc@hf.com>
2015-09-30 00:25:32 -0500

    added Naturtint-Permanent-Hair

--- processing/Naturtint-Permanent-Hair-color.ps1
+++ processing/Naturtint-Permanent-Hair-color.ps1
@@ -1,5 +1,5 @@
 ﻿. "d:\Data\gitrepos\atjshop.github.io\processing\general-html.for.multiple.products.ps1"
 $imgsubdirs=@('Naturtint-Permanent-Hair')
 $htmlfilename='Naturtint-Permanent-Hair.html'
-$title = '天然无毒有机染发剂'
+$title = '美国第一品牌天然无毒永久性染发剂'
 generate-html-for-products $imgsubdirs $htmlfilename $title
