bc8073570083c1cfacbcb9535868460c13648db6
hf <hfc@hf.com>
2015-10-03 14:23:50 -0500

    added philips-shaver.html

---
+++ processing/philips-shaver.ps1
@@ -0,0 +1,5 @@
+﻿. "d:\Data\gitrepos\atjshop.github.io\processing\general-html.for.multiple.products.ps1"
+$imgsubdirs=@('philips-shaver-3d-9400','philips-shaver-4600')
+$htmlfilename='philips-shaver.html'
+$title = '荷兰产飞利浦3D三刀头电动剃须刀系列'
+generate-html-for-products $imgsubdirs $htmlfilename $title 1
