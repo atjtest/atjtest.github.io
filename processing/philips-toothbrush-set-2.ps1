ed7868f974357a8e0369c9f46b6b6971106d01f2
hf <hfc@hf.com>
2015-10-03 17:18:05 -0500

    added philips-toothbrush-set-2

---
+++ processing/philips-toothbrush-set-2.ps1
@@ -0,0 +1,6 @@
+﻿. "d:\Data\gitrepos\atjshop.github.io\processing\general-html.for.multiple.products.ps1"
+$imgsubdirs=@('philips-toothbrush')
+$htmlfilename='philips-toothbrush.html'
+$title = '飞利浦电动牙刷2支装豪华套装'
+# generate-html-for-products $imgsubdirs $htmlfilename $title 1
+generate-html-for-products $imgsubdirs $htmlfilename $title
