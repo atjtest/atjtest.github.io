521603a1b74ece4b9dcf7025f95b8c66d7694d71
hf <hfc@hf.com>
2015-10-13 22:42:18 -0500

    added EL giftset

--- processing/2015-10-EL.ps1
+++ processing/2015-10-EL.ps1
@@ -1,5 +1,5 @@
 ﻿. "d:\Data\gitrepos\atjshop.github.io\processing\general-html.for.multiple.products.ps1"
-$imgsubdirs=@('2015-09-EL-1','2015-09-EL-2','2015-09-EL-3','2015-09-EL-4','2015-09-EL-5','2015-09-EL-6','2015-09-EL-7')
+$imgsubdirs=@('2015-09-EL-1','2015-09-EL-2','2015-09-EL-3','2015-09-EL-4','2015-09-EL-5','2015-09-EL-6','2015-09-EL-7','2015-09-EL-Giftset')
 $htmlfilename='2015-09-EL-SET.html'
 $title = '2015年10月份雅诗兰黛超值套装'
 generate-html-for-products $imgsubdirs $htmlfilename $title
