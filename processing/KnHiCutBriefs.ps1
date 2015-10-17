a2c6b97d979d433a058bf648961f6db4e5584851
hf <hfc@hf.com>
2015-10-17 18:42:12 -0500

    added KnHiCutBriefs

---
+++ processing/KnHiCutBriefs.ps1
@@ -0,0 +1,5 @@
+﻿. "d:\Data\gitrepos\atjshop.github.io\processing\general-html.for.multiple.products.ps1"
+$imgsubdirs=@('KnHiCutBriefs')
+$htmlfilename='KnHiCutBriefs.html'
+$title = 'karen neuburger 6条装成人女生蕾丝边内裤'
+generate-html-for-products $imgsubdirs $htmlfilename $title
