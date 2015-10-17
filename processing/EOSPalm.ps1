96fa845267c1c0980df30cdd848b3f8cfb313a3f
hf <hfc@hf.com>
2015-10-17 18:17:53 -0500

    added EOSOrganicLipBalm.html

---
+++ processing/EOSPalm.ps1
@@ -0,0 +1,5 @@
+﻿. "d:\Data\gitrepos\atjshop.github.io\processing\general-html.for.multiple.products.ps1"
+$imgsubdirs=@('EOSOrganicLipBalm')
+$htmlfilename='EOSOrganicLipBalm.html'
+$title = '又萌又漂亮eos伊欧诗天然有机润唇膏5个水果味球装'
+generate-html-for-products $imgsubdirs $htmlfilename $title
